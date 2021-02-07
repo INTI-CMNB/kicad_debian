63df0318710eef5a82c8d31cb687ef37de3f3fc8
Salvador E. Tropea <salvador@inti.gob.ar>
2021-02-07 11:23:21 -0300

    Trying to figure out what mess did docker hub.

--- build-nightly.sh
+++ build-nightly.sh
@@ -2,5 +2,5 @@
 # KiCad nightly doesn't include the global symbol lib table:
 #cp /usr/share/kicad/template/sym-lib-table .
 #cp /usr/share/kicad/template/fp-lib-table .
-docker build -f Dockerfile-nightly -t setsoft/kicad_debian:bullseye-5.99-20200922 .
+docker build --no-cache -f Dockerfile-nightly -t setsoft/kicad_debian:bullseye-5.99-20201013 .
 docker build -f Dockerfile-nightly -t setsoft/kicad_debian:nightly .
