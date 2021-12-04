9477860ff1b7eede3390220a54e184bc2c617b08
Salvador E. Tropea <salvador@inti.gob.ar>
2021-12-04 14:45:53 -0300

    Updated nightly build script

--- build-nightly.sh
+++ build-nightly.sh
@@ -2,5 +2,5 @@
 # KiCad nightly doesn't include the global symbol lib table:
 #cp /usr/share/kicad/template/sym-lib-table .
 #cp /usr/share/kicad/template/fp-lib-table .
-docker build --no-cache -f Dockerfile-nightly -t setsoft/kicad_debian:bullseye-5.99-20210408 .
+docker build --no-cache -f Dockerfile-nightly -t setsoft/kicad_debian:bullseye-6.0.0rc1-20211202 .
 docker build -f Dockerfile-nightly -t setsoft/kicad_debian:nightly .
