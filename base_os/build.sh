b069fcbc1d6d5fe5770263e68b8cd375dc7cf032
Salvador E. Tropea <salvador@inti.gob.ar>
2023-03-14 07:46:44 -0300

    Fixed images references

--- base_os/build.sh
+++ base_os/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker build -f Dockerfile -t ghcr.io/inti-cmnb/base_os:latest .
-TG=d`docker run --rm ghcr.io/inti-cmnb/base_os:d11 cat /etc/debian_version | tr -d '\n'`
+TG=d`docker run --rm ghcr.io/inti-cmnb/base_os:latest cat /etc/debian_version | tr -d '\n'`
 docker tag ghcr.io/inti-cmnb/base_os:latest ghcr.io/inti-cmnb/base_os:${TG}
 docker push ghcr.io/inti-cmnb/base_os:latest
 docker push ghcr.io/inti-cmnb/base_os:${TG}
