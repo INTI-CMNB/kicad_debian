826f23fe36cb2eb027fb54c2b10abb92de472c5f
Salvador E. Tropea <salvador@inti.gob.ar>
2023-03-14 13:07:02 -0300

    Fixed KiCad 5 full tags

--- ki5_full/build.sh
+++ ki5_full/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad5_debian:latest_full .
-TG1=`docker run --rm ghcr.io/inti-cmnb/kicad6_debian:latest_full kicad_version.py`
-TG2=d`docker run --rm ghcr.io/inti-cmnb/kicad6_debian:latest_full cat /etc/debian_version | tr -d '\n'`
-TG3=b`docker run --rm ghcr.io/inti-cmnb/kicad6_debian:latest_full /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
+TG1=`docker run --rm ghcr.io/inti-cmnb/kicad5_debian:latest_full kicad_version.py`
+TG2=d`docker run --rm ghcr.io/inti-cmnb/kicad5_debian:latest_full cat /etc/debian_version | tr -d '\n'`
+TG3=b`docker run --rm ghcr.io/inti-cmnb/kicad5_debian:latest_full /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
 docker tag ghcr.io/inti-cmnb/kicad5_debian:latest_full ghcr.io/inti-cmnb/kicad_debian:${TG1}-${TG2}-${TG3}
 docker tag ghcr.io/inti-cmnb/kicad5_debian:latest_full ghcr.io/inti-cmnb/kicad_debian:latest_full
 docker tag ghcr.io/inti-cmnb/kicad5_debian:latest_full ghcr.io/inti-cmnb/kicad5_debian:${TG1}_${TG2}_${TG3}
