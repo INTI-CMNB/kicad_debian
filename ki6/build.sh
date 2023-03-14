91e43c40736988df17c12ca9aa19cce429fb6130
Salvador E. Tropea <salvador@inti.gob.ar>
2023-03-14 08:49:53 -0300

    Adapted KiCad 6 image generation

--- ki6/build.sh
+++ ki6/build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-# KiCad nightly doesn't include the global symbol lib table:
-#cp /usr/share/kicad/template/sym-lib-table .
-#cp /usr/share/kicad/template/fp-lib-table .
-docker build --no-cache -f Dockerfile -t setsoft/kicad_debian:ki6 .
-docker build -f Dockerfile-nightly -t setsoft/kicad_debian:ki6.0.0_Ubuntu21.10 .
+docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad6_debian:latest .
+TG1=d`docker run --rm ghcr.io/inti-cmnb/kicad6_debian:latest kicad_version.py`
+TG2=d`docker run --rm ghcr.io/inti-cmnb/kicad6_debian:latest cat /etc/debian_version | tr -d '\n'`
+docker tag ghcr.io/inti-cmnb/kicad6_debian:latest ghcr.io/inti-cmnb/kicad_debian:ki6
+docker tag ghcr.io/inti-cmnb/kicad6_debian:latest ghcr.io/inti-cmnb/kicad6_debian:${TG1}
+docker tag ghcr.io/inti-cmnb/kicad6_debian:latest ghcr.io/inti-cmnb/kicad6_debian:${TG1}_${TG2}
+docker push ghcr.io/inti-cmnb/kicad6_debian:latest
+docker push ghcr.io/inti-cmnb/kicad6_debian:${TG1}
+docker push ghcr.io/inti-cmnb/kicad6_debian:${TG1}_${TG2}
+docker push ghcr.io/inti-cmnb/kicad_debian:ki6
