cf3d70a0a74e4d3254524e0764e67b9db93d920f
Salvador E. Tropea <salvador@inti.gob.ar>
2023-03-15 11:24:58 -0300

    Added KiCad 7 docker files

---
+++ ki7/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cp ../kicad_version.py .
+docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad7_debian:latest .
+TG1=`docker run --rm ghcr.io/inti-cmnb/kicad7_debian:latest kicad_version.py`
+TG2=d`docker run --rm ghcr.io/inti-cmnb/kicad7_debian:latest cat /etc/debian_version | tr -d '\n'`
+docker tag ghcr.io/inti-cmnb/kicad7_debian:latest ghcr.io/inti-cmnb/kicad_debian:ki${TG1}_Debian
+docker tag ghcr.io/inti-cmnb/kicad7_debian:latest ghcr.io/inti-cmnb/kicad7_debian:${TG1}
+docker tag ghcr.io/inti-cmnb/kicad7_debian:latest ghcr.io/inti-cmnb/kicad7_debian:${TG1}_${TG2}
+docker push ghcr.io/inti-cmnb/kicad_debian:ki${TG1}_Debian
+docker push ghcr.io/inti-cmnb/kicad7_debian:${TG1}
+docker push ghcr.io/inti-cmnb/kicad7_debian:${TG1}_${TG2}
+docker push ghcr.io/inti-cmnb/kicad7_debian:latest
+
---
+++ ki7_full/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cp ../kicad_version.py .
+docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad7_debian:latest_full .
+TG1=`docker run --rm ghcr.io/inti-cmnb/kicad7_debian:latest_full kicad_version.py`
+TG2=d`docker run --rm ghcr.io/inti-cmnb/kicad7_debian:latest_full cat /etc/debian_version | tr -d '\n'`
+TG3=b`docker run --rm ghcr.io/inti-cmnb/kicad7_debian:latest_full /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
+docker tag ghcr.io/inti-cmnb/kicad7_debian:latest_full ghcr.io/inti-cmnb/kicad_debian:ki${TG1}_Debian_full
+docker tag ghcr.io/inti-cmnb/kicad7_debian:latest_full ghcr.io/inti-cmnb/kicad7_debian:${TG1}_${TG2}_${TG3}
+docker push ghcr.io/inti-cmnb/kicad_debian:ki${TG1}_Debian_full
+docker push ghcr.io/inti-cmnb/kicad7_debian:${TG1}_${TG2}_${TG3}
+docker push ghcr.io/inti-cmnb/kicad7_debian:latest_full
+
