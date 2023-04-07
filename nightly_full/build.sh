9852077504760b870390a1d365115d92ef3101fa
Salvador E. Tropea <salvador@inti.gob.ar>
2023-04-07 13:58:59 -0300

    Added specific tag for full nightly

--- nightly_full/build.sh
+++ nightly_full/build.sh
@@ -5,5 +5,6 @@ TG1=`docker run --rm ghcr.io/inti-cmnb/kicad_debian:nightly_full kicad_version.p
 TG2=`docker run --rm ghcr.io/inti-cmnb/kicad_debian:nightly_full /bin/bash -c "grep DISTRIB_CODENAME /etc/lsb-release | sed  s/DISTRIB_CODENAME=//" | tr -d '\n'`
 TG3=b`docker run --rm ghcr.io/inti-cmnb/kicad_debian:nightly_full /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
 docker tag ghcr.io/inti-cmnb/kicad_debian:nightly_full ghcr.io/inti-cmnb/kicad_debian:${TG1}_${TG2}_${TG3}
+docker push ghcr.io/inti-cmnb/kicad_debian:${TG1}_${TG2}_${TG3}
 docker push ghcr.io/inti-cmnb/kicad_debian:nightly_full
 
