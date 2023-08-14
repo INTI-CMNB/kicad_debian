3f5a8684085056c7c396697f6b802843b7206376
Salvador E. Tropea <salvador@inti.gob.ar>
2023-08-14 09:29:09 -0300

    Better Blender version detection
    - Works for the Debian package

--- ki6_full/build.sh
+++ ki6_full/build.sh
@@ -3,7 +3,7 @@ cp ../kicad_version.py .
 docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad6_debian:latest_full .
 TG1=`docker run --rm ghcr.io/inti-cmnb/kicad6_debian:latest_full kicad_version.py`
 TG2=d`docker run --rm ghcr.io/inti-cmnb/kicad6_debian:latest_full cat /etc/debian_version | tr -d '\n'`
-TG3=b`docker run --rm ghcr.io/inti-cmnb/kicad6_debian:latest_full /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
+TG3=b`docker run --rm ghcr.io/inti-cmnb/kicad6_debian:latest_full /bin/bash -c "blender --version | head -n 1 | tr -d 'Blender '"`
 docker tag ghcr.io/inti-cmnb/kicad6_debian:latest_full ghcr.io/inti-cmnb/kicad_debian:ki${TG1}_Debian_full
 docker tag ghcr.io/inti-cmnb/kicad6_debian:latest_full ghcr.io/inti-cmnb/kicad6_debian:${TG1}_${TG2}_${TG3}
 docker push ghcr.io/inti-cmnb/kicad_debian:ki${TG1}_Debian_full
--- stable_nightly_full/build.sh
+++ stable_nightly_full/build.sh
@@ -3,7 +3,7 @@ cp ../kicad_version.py .
 docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad_debian:stable_nightly_full .
 TG1=t`docker run --rm ghcr.io/inti-cmnb/kicad_debian:stable_nightly_full kicad_version.py`
 TG2=`docker run --rm ghcr.io/inti-cmnb/kicad_debian:stable_nightly_full /bin/bash -c "grep DISTRIB_CODENAME /etc/lsb-release | sed  s/DISTRIB_CODENAME=//" | tr -d '\n'`
-TG3=b`docker run --rm ghcr.io/inti-cmnb/kicad_debian:stable_nightly_full /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
+TG3=b`docker run --rm ghcr.io/inti-cmnb/kicad_debian:stable_nightly_full /bin/bash -c "blender --version | head -n 1 | tr -d 'Blender '"`
 docker tag ghcr.io/inti-cmnb/kicad_debian:stable_nightly_full ghcr.io/inti-cmnb/kicad_debian:${TG1}_${TG2}_${TG3}
 docker push ghcr.io/inti-cmnb/kicad_debian:${TG1}_${TG2}_${TG3}
 docker push ghcr.io/inti-cmnb/kicad_debian:stable_nightly_full
