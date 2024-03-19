b44fc9279e2913ed9fd2678eadbaaeb262f89a8b
Salvador E. Tropea <salvador@inti.gob.ar>
2024-03-19 07:02:50 -0300

    Added KiCad 8 Full using Sid

---
+++ ki8_full/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+cp ../kicad_version.py .
+docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad8_debian:latest_full .
+TG1=`docker run --rm ghcr.io/inti-cmnb/kicad8_debian:latest_full kicad_version.py`
+TG2=d_sid
+TG3=b`docker run --rm ghcr.io/inti-cmnb/kicad8_debian:latest_full /bin/bash -c "blender --version | head -n 1 | tr -d 'Blender '"`
+docker tag ghcr.io/inti-cmnb/kicad8_debian:latest_full ghcr.io/inti-cmnb/kicad_debian:ki${TG1}_Debian_full
+docker tag ghcr.io/inti-cmnb/kicad8_debian:latest_full ghcr.io/inti-cmnb/kicad8_debian:${TG1}_${TG2}_${TG3}
+docker push ghcr.io/inti-cmnb/kicad_debian:ki${TG1}_Debian_full
+docker push ghcr.io/inti-cmnb/kicad8_debian:${TG1}_${TG2}_${TG3}
+docker push ghcr.io/inti-cmnb/kicad8_debian:latest_full
+
