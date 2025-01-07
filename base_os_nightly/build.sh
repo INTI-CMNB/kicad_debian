5019f34a3656bf6da7adcd0cf509825be8f19e6c
Salvador E. Tropea <salvador@inti.gob.ar>
2025-01-07 06:39:48 -0300

    [Nightly] KiCad 9.0.0 RC1

--- base_os_nightly/build.sh
+++ base_os_nightly/build.sh
@@ -2,5 +2,5 @@
 set -e
 cp ../base_os/dl_deb.py .
 docker build -f Dockerfile -t ghcr.io/inti-cmnb/base_os:latest_nightly .
-TG=d`docker run --rm ghcr.io/inti-cmnb/base_os:latest_nightly cat /etc/debian_version | tr -d '\n'`
-docker tag ghcr.io/inti-cmnb/base_os:latest_nightly ghcr.io/inti-cmnb/base_os:${TG}
+# TG=d`docker run --rm ghcr.io/inti-cmnb/base_os:latest_nightly cat /etc/debian_version | tr -d '\n'`
+# docker tag ghcr.io/inti-cmnb/base_os:latest_nightly ghcr.io/inti-cmnb/base_os:${TG}
--- nightly/build.sh
+++ nightly/build.sh
@@ -3,5 +3,6 @@ set -e
 cp ../kicad_version.py .
 docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad_debian:nightly .
 TG1=`docker run --rm ghcr.io/inti-cmnb/kicad_debian:nightly kicad_version.py`
-TG2=`docker run --rm ghcr.io/inti-cmnb/kicad_debian:nightly /bin/bash -c "grep DISTRIB_CODENAME /etc/lsb-release | sed  s/DISTRIB_CODENAME=//" | tr -d '\n'`
+# TG2=`docker run --rm ghcr.io/inti-cmnb/kicad_debian:nightly /bin/bash -c "grep DISTRIB_CODENAME /etc/lsb-release | sed  s/DISTRIB_CODENAME=//" | tr -d '\n'`
+TG2=d`docker run --rm ghcr.io/inti-cmnb/kicad_debian:nightly cat /etc/debian_version | tr -d '\n'`
 docker tag ghcr.io/inti-cmnb/kicad_debian:nightly ghcr.io/inti-cmnb/kicad_debian:${TG1}_${TG2}
