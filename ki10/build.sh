18081972bf09fb013b762841dbc9606601c0fca7
Salvador E. Tropea <salvador@inti.gob.ar>
2026-04-10 11:22:36 -0300

    [Fixed][KiCad 10] Build script (9 -> 10)

--- ki10/build.sh
+++ ki10/build.sh
@@ -5,8 +5,8 @@ docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad10_debian:latest .
 TG1=`docker run --rm ghcr.io/inti-cmnb/kicad10_debian:latest kicad_version.py`
 TG2=d`docker run --rm ghcr.io/inti-cmnb/kicad10_debian:latest cat /etc/debian_version | tr -d '\n'`
 docker tag ghcr.io/inti-cmnb/kicad10_debian:latest ghcr.io/inti-cmnb/kicad_debian:ki${TG1}_Debian
-docker tag ghcr.io/inti-cmnb/kicad10_debian:latest ghcr.io/inti-cmnb/kicad_debian:ki9
+docker tag ghcr.io/inti-cmnb/kicad10_debian:latest ghcr.io/inti-cmnb/kicad_debian:ki10
 docker tag ghcr.io/inti-cmnb/kicad10_debian:latest setsoft/kicad_debian:ki${TG1}_Debian
-docker tag ghcr.io/inti-cmnb/kicad10_debian:latest setsoft/kicad_debian:ki9
+docker tag ghcr.io/inti-cmnb/kicad10_debian:latest setsoft/kicad_debian:ki10
 docker tag ghcr.io/inti-cmnb/kicad10_debian:latest ghcr.io/inti-cmnb/kicad10_debian:${TG1}
 docker tag ghcr.io/inti-cmnb/kicad10_debian:latest ghcr.io/inti-cmnb/kicad10_debian:${TG1}_${TG2}
